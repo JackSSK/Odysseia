d2677edef17631ff95527ffdea7512948cfde2e3
JackSSK <angrayg@gmail.com>
2023-03-30 13:43:01 +0800

    modify scripts

--- train.ps1
+++ train.ps1
@@ -1,61 +1,82 @@
-# LoRA train script by @Akegarasu
+# Activate python venv
+.\sd-scripts\venv\Scripts\activate
 
-$a = "stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned-emaonly.ckpt" 
 $n_workers = 1
+$model_name = "glock_design_v0"
+$pretrained_model = "stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned-emaonly.ckpt"  
+$train_data_dir = "data/Glock17/img/" 
+$reg_data_dir = "" 
 
-# Train data path | 设置训练用模型、图片
-$pretrained_model = $a # base model path | 底模路径
-$train_data_dir = "data/img" # train dataset path | 训练数据集路径
-$reg_data_dir = "" # directory for regularization images | 正则化数据集路径，默认不使用正则化图像。
 
-# Network settings | 网络设置
-$network_module = "networks.lora" # 在这里将会设置训练的网络种类，默认为 networks.lora 也就是 LoRA 训练。如果你想训练 LyCORIS（LoCon、LoHa） 等，则修改这个值为 lycoris.kohya
-$network_weights = "" # pretrained weights for LoRA network | 若需要从已有的 LoRA 模型上继续训练，请填写 LoRA 模型路径。
-$network_dim = 32 # network dim | 常用 4~128，不是越大越好
-$network_alpha = 32 # network alpha | 常用与 network_dim 相同的值或者采用较小的值，如 network_dim的一半 防止下溢。默认值为 1，使用较小的 alpha 需要提升学习率。
+################################## Network settings ##################################
 
-# Train related params | 训练相关参数
-$resolution = "512,512" # image resolution w,h. 图片分辨率，宽,高。支持非正方形，但必须是 64 倍数。
-$batch_size = 1 # batch size
-$max_train_epoches = 10 # max train epoches | 最大训练 epoch
-$save_every_n_epochs = 2 # save every n epochs | 每 N 个 epoch 保存一次
+# networks.lora -> LoRA; lycoris.kohya -> LyCORIS（LoCon、LoHa）
+$network_module = "networks.lora" 
 
-$train_unet_only = 0 # train U-Net only | 仅训练 U-Net，开启这个会牺牲效果大幅减少显存使用。6G显存可以开启
-$train_text_encoder_only = 0 # train Text Encoder only | 仅训练 文本编码器
+# pretrained weights for LoRA network
+$network_weights = "" 
 
-$noise_offset = 0 # noise offset | 在训练中添加噪声偏移来改良生成非常暗或者非常亮的图像，如果启用，推荐参数为 0.1
-$keep_tokens = 0 # keep heading N tokens when shuffling caption tokens | 在随机打乱 tokens 时，保留前 N 个不变。
+# 4~128
+$network_dim = 32 
 
-# Learning rate | 学习率
+# network alpha，similar with network dim or small values.
+# Consider increase learning ratio if using small values
+$network_alpha = 32 
+
+
+################################## Train related params ##################################
+# 64i, 64j
+$resolution = "512,512"
+$batch_size = 1 
+$max_train_epoches = 10 
+$save_every_n_epochs = 2 
+$train_unet_only = 0 
+$train_text_encoder_only = 0 
+
+# Modify offset value if having outputs too bright or too dark: ~0.1
+$noise_offset = 0 
+# keep heading N tokens when shuffling caption tokens
+$keep_tokens = 0 
+
+
+################################## Learning ratio ##################################
 $lr = "1e-4"
 $unet_lr = "1e-4"
 $text_encoder_lr = "1e-5"
-$lr_scheduler = "cosine_with_restarts" # "linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup"
-$lr_warmup_steps = 0 # warmup steps | 仅在 lr_scheduler 为 constant_with_warmup 时需要填写这个值
-$lr_restart_cycles = 1 # cosine_with_restarts restart cycles | 余弦退火重启次数，仅在 lr_scheduler 为 cosine_with_restarts 时起效。
+# "linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup"
+$lr_scheduler = "cosine_with_restarts" 
+$lr_warmup_steps = 0
+$lr_restart_cycles = 1
 
-# Output settings | 输出设置
-$output_name = "glock17_v0" # output model name | 模型保存名称
-$save_model_as = "safetensors" # model save ext | 模型保存格式 ckpt, pt, safetensors
 
-# 其他设置
-$min_bucket_reso = 256 # arb min resolution | arb 最小分辨率
-$max_bucket_reso = 1024 # arb max resolution | arb 最大分辨率
-$persistent_data_loader_workers = 0 # persistent dataloader workers | 容易爆内存，保留加载训练集的worker，减少每个 epoch 之间的停顿
-$clip_skip = 2 # clip skip | 玄学 一般用 2
+################################## Optimizer params ##################################
+$use_8bit_adam = 1 # use 8bit adam optimizer?
+$use_lion = 0 # use lion optimizer?
 
-# 优化器设置
-$use_8bit_adam = 1 # use 8bit adam optimizer | 使用 8bit adam 优化器节省显存，默认启用。部分 10 系老显卡无法使用，修改为 0 禁用。
-$use_lion = 0 # use lion optimizer | 使用 Lion 优化器
 
-# LyCORIS 训练设置
-$algo = "lora" # LyCORIS network algo | LyCORIS 网络算法 可选 lora、loha。lora即为locon
-$conv_dim = 4 # conv dim | 类似于 network_dim，推荐为 4
-$conv_alpha = 4 # conv alpha | 类似于 network_alpha，可以采用与 conv_dim 一致或者更小的值
+################################## LyCORIS params ##################################
+# LyCORIS network algo: lora / loha 
+$algo = "lora" 
+$conv_dim = 4
+$conv_alpha = 4
 
-# ============= DO NOT MODIFY CONTENTS BELOW | 请勿修改下方内容 =====================
-# Activate python venv
-.\sd-scripts\venv\Scripts\activate
+
+################################## Output settings ##################################
+$output_name = $model_name
+# ckpt, pt, safetensors
+$save_model_as = "safetensors"
+
+
+################################## Others ##################################
+# arb min resolution
+$min_bucket_reso = 256 
+
+# arb max resolution
+$max_bucket_reso = 1024 
+
+$persistent_data_loader_workers = 0 
+$clip_skip = 2 
+
 
 $Env:HF_HOME = "huggingface"
 $ext_args = [System.Collections.ArrayList]::new()
